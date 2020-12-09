60920f2f3b3c9a5f46d06e6cf8945b4e709aa881
Thomas Kite <thomas.kite@student.manchester.ac.uk>
2020-12-09 08:37:26 +0000

    Day 9 in python

---
+++ python/headers/day9.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Day 8 of Advent of Code 2020
+
+
+Tom Kite - 03/12/2020
+"""
+
+
+def readfile(filename):
+    with open(filename, 'r') as file:
+        lines = [int(line.strip()) for line in file]
+    return lines
+
+
+def sum_in_list(arr, total):
+    rval = [x for x in arr if total-x in arr and x != total/2]
+    return bool(len(rval))
+
+
+def find_first_break(arr, pream_len=25):
+    for i, num in enumerate(arr[pream_len:]):
+        if not sum_in_list(arr[i: i+pream_len], num):
+            return num
+    return False
+
+
+def contiguous_vals(arr, total):
+    for size in range(len(arr)):
+        for pos in range(len(arr)-size):
+            test = arr[pos:pos+size]
+            if sum(test) == total:
+                return test
+    return False
+
+
+def part1(filename):
+    data = readfile(filename)
+    answer = find_first_break(data)
+    print("First number without a previous sum is {}.".format(answer))
+    return
+
+
+def part2(filename):
+    data = readfile(filename)
+    num_to_find = find_first_break(data)
+    search_arr = [x for x in data if x != num_to_find]
+    arr_found = contiguous_vals(search_arr, num_to_find)
+    print("The sum of biggest and smallest values in contigous list is {}."
+          .format(max(arr_found) + min(arr_found)))
+    return
+
+
+if __name__ == "__main__":
+    print("Part 1:")
+    part1("../../data/day9.dat")
+    print("\nPart 2:")
+    part2("../../data/day9.dat")
--- python/main.py
+++ python/main.py
@@ -7,7 +7,7 @@ This script will serve as a main menu which calls other days
 Tom Kite - 03/12/2020
 """
 
-from headers import day1, day2, day3, day4, day5, day6, day7, day8
+from headers import day1, day2, day3, day4, day5, day6, day7, day8, day9
 
 
 menu_options = {1: day1,
@@ -17,7 +17,8 @@ menu_options = {1: day1,
                 5: day5,
                 6: day6,
                 7: day7,
-                8: day8}
+                8: day8,
+                9: day9}
 
 
 def int_input(entry):
