b007128f2e72718e7bd595c7975124d2dd1b3bcb
Tom Kite <tomkite57@gmail.com>
2020-12-13 16:22:38 +0000

    day 13 in python

---
+++ python/headers/day13.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Day 11 of Advent of Code 2020
+
+
+Tom Kite - 03/12/2020
+"""
+
+import numpy as np
+
+
+def readfile(filename):
+    with open(filename, 'r') as file:
+        lines = [line.strip() for line in file]
+    return lines
+
+
+def process_v1(data):
+    return int(data[0]), [int(x) for x in data[1].split(',') if x != 'x']
+
+
+def process_v2(data):
+    return [int(x) if x != 'x' else x for x in data[1].split(',')]
+
+
+def wait_time(arriv_time, bus_time):
+    delay = (arriv_time % bus_time)
+    if delay != 0:
+        return bus_time - (arriv_time % bus_time)
+    return 0
+
+
+def sync_to_bus(bus_time, bus_delay, current_time, delta_time):
+    while wait_time(current_time, bus_time) != (bus_delay % bus_time):
+        current_time += delta_time
+    return current_time
+
+
+def sync_all_busses(bus_data):
+    current_time = np.array([0], dtype='uint64')
+    lcm = np.array([1], dtype='uint64')
+    for i, bus in enumerate(bus_data):
+        if bus == 'x':
+            continue
+        current_time = sync_to_bus(bus, i, current_time, lcm)
+        lcm = np.lcm(lcm, bus)
+    return current_time[0]
+
+
+def part1(filename):
+    arrival_time, bus_times = process_v1(readfile(filename))
+    best_bus_time = min(bus_times, key=lambda x: wait_time(arrival_time, x))
+    print("Bus ID * wait time is {}."
+          .format(best_bus_time*wait_time(arrival_time, best_bus_time)))
+    return
+
+
+def part2(filename):
+    bus_data = process_v2(readfile(filename))
+    time = sync_all_busses(bus_data)
+    print("First time stamp is {}.".format(time))
+    return
+
+
+if __name__ == "__main__":
+    print("Part 1:")
+    part1("../../data/day13.dat")
+    print("\nPart 2:")
+    part2("../../data/day13.dat")
--- python/main.py
+++ python/main.py
@@ -8,12 +8,12 @@ Tom Kite - 03/12/2020
 """
 
 from headers import day1, day2, day3, day4, day5, day6, day7, day8, day9,\
-                    day10, day11, day12
+                    day10, day11, day12, day13
 
 
 menu_options = {1: day1, 2: day2, 3: day3, 4: day4, 5: day5,
                 6: day6, 7: day7, 8: day8, 9: day9, 10: day10,
-                11: day11, 12: day12}
+                11: day11, 12: day12, 13: day13}
 
 
 def int_input(entry):
