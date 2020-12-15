7e9eee4e4de146f1329d584323b083c0411ef0e7
Thomas Kite <thomas.kite@student.manchester.ac.uk>
2020-12-15 15:41:45 +0000

    Day 15 complete in Python. Yay! :)

---
+++ python/headers/day15.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Day 15 of Advent of Code 2020
+
+
+Tom Kite - 03/12/2020
+"""
+
+
+def readfile(filename):
+    with open(filename, 'r') as file:
+        nums = [line.split(',') for line in file][0]
+    return [int(x) for x in nums]
+
+
+class memory_game():
+    def __init__(self, start_nums):
+        self.last_turn = {}
+        self.initial_nums = start_nums
+        self.counter = 0
+        for num in self.initial_nums:
+            self.last_turn[num] = [self.counter, ]
+            self.counter += 1
+        self.prev_num = self.initial_nums[-1]
+
+    def evolve_till(self, N):
+        while self.counter < N:
+            if len(self.last_turn[self.prev_num]) < 2:
+                num = 0
+            else:
+                turns = self.last_turn[self.prev_num]
+                num = turns[-1] - turns[-2]
+
+            if num in self.last_turn.keys():
+                self.last_turn[num].append(self.counter)
+                self.last_turn[num] = self.last_turn[num][-2:]
+            else:
+                self.last_turn[num] = [self.counter, ]
+            self.prev_num = num
+            self.counter += 1
+        return num
+
+
+def part1(filename):
+    data = readfile(filename)
+    game = memory_game(data)
+    final_num = game.evolve_till(2020)
+    print("Final number spoken is {}.".format(final_num))
+
+
+def part2(filename):
+    data = readfile(filename)
+    game = memory_game(data)
+    final_num = game.evolve_till(30000000)
+    print("Final number spoken is {}.".format(final_num))
+
+
+if __name__ == "__main__":
+    print("Part 1:")
+    part1("../../data/day15.dat")
+    print("\nPart 2:")
+    part2("../../data/day15.dat")
