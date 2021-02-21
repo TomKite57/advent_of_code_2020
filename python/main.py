7f34c7aa067686bf979e8b25750ace87afd46b27
Tom Kite <tomkite57@gmail.com>
2021-02-21 23:15:10 +0000

    Added day 25 to my main file

--- python/main.py
+++ python/main.py
@@ -10,14 +10,14 @@ Tom Kite - 25/12/2020
 from aoc_tools.advent_timer import Advent_Timer
 from headers import day1, day2, day3, day4, day5, day6, day7, day8, day9,\
                     day10, day11, day12, day13, day14, day15, day16, day17,\
-                    day18, day19, day20, day21, day22, day23, day24
+                    day18, day19, day20, day21, day22, day23, day24, day25
 
 
 menu_options = {1: day1, 2: day2, 3: day3, 4: day4, 5: day5,
                 6: day6, 7: day7, 8: day8, 9: day9, 10: day10,
                 11: day11, 12: day12, 13: day13, 14: day14, 15: day15,
                 16: day16, 17: day17, 18: day18, 19: day19, 20: day20,
-                21: day21, 22: day22, 23: day23, 24: day24}
+                21: day21, 22: day22, 23: day23, 24: day24, 25: day25}
 
 
 def all_days():
